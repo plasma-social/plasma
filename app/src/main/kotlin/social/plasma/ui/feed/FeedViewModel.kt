f2d57b0e7a208f4853977eb9f69a9013b4243046
Jem Mawson <jem.mawson@gmail.com>
2023-01-09 22:20:25 +1000

    Redo "Split subscription call and notes flow" (#19)

--- app/src/main/kotlin/social/plasma/ui/feed/FeedViewModel.kt
+++ app/src/main/kotlin/social/plasma/ui/feed/FeedViewModel.kt
@@ -1,6 +1,5 @@
 package social.plasma.ui.feed
 
-import android.util.Log
 import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,9 +8,10 @@ import app.cash.molecule.RecompositionClock
 import app.cash.molecule.launchMolecule
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import social.plasma.models.Note
 import social.plasma.relay.Relays
 import javax.inject.Inject
@@ -24,16 +24,22 @@ class FeedViewModel @Inject constructor(
     private val moleculeScope =
         CoroutineScope(viewModelScope.coroutineContext + AndroidUiDispatcher.Main)
 
+    val relay = relays.relay("wss://relay.damus.io")
+
     // TODO only subscribe to relays once during the entire app cycle
     // https://github.com/nostr-protocol/nips/blob/master/01.md#other-notes
-    private val relaySubscription: Flow<Note> = runBlocking {
-        relays.relay("wss://relay.damus.io").connectAndSubscribe().flowNotes()
-    }
+    private val relaySubscription: Flow<Note> = relay.flowNotes()
 
     val uiState: StateFlow<FeedListUiState> = moleculeScope.launchMolecule(recompositionClock) {
         // TODO move this to some type of repository that can aggregate the list
         val messageList = remember { mutableStateListOf<Note>() }
         val newMessage by relaySubscription.collectAsState(initial = null)
+
+        LaunchedEffect(Unit) {
+            withContext(Dispatchers.IO) {
+                relay.connectAndSubscribe()
+            }
+        }
 
         LaunchedEffect(newMessage) {
             newMessage?.let { messageList.add(0, it) }
