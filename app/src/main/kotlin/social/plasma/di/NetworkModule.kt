165e4b73e8faf0ee2ee1d8bacee3384b27f05dd7
Jose Mateo <jmateo6@gmail.com>
2023-01-09 21:52:48 -0500

    First pass of notes repository that aggregates multiple relays

--- app/src/main/kotlin/social/plasma/di/NetworkModule.kt
+++ app/src/main/kotlin/social/plasma/di/NetworkModule.kt
@@ -1,20 +1,41 @@
 package social.plasma.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.streamadapter.rxjava2.RxJava2StreamAdapterFactory
+import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
-import social.plasma.relay.Relays
+import okhttp3.logging.HttpLoggingInterceptor
+import social.plasma.relay.message.NostrMessageAdapter
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Provides
     @Singleton
-    fun provideRelays(): Relays {
-        return Relays(OkHttpClient())
-    }
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi = Moshi.Builder()
+        .add(NostrMessageAdapter())
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesScarletBuilder(moshi: Moshi): Scarlet.Builder = Scarlet.Builder()
+        .addMessageAdapterFactory(MoshiMessageAdapter.Factory(moshi))
+        .addStreamAdapterFactory(RxJava2StreamAdapterFactory())
+        .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
 }
--- app/src/test/kotlin/social/plasma/relay/RelayTest.kt
+++ app/src/test/kotlin/social/plasma/relay/RelayTest.kt
@@ -1,22 +1,38 @@
 package social.plasma.relay
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.streamadapter.rxjava2.RxJava2StreamAdapterFactory
+import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import social.plasma.relay.Relays
+import social.plasma.relay.message.NostrMessageAdapter
 
 class RelayTest : StringSpec({
 
     "can get events from a relay" {
-        val relays = Relays(OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+        val moshi = Moshi.Builder()
+            .add(NostrMessageAdapter())
+            .addLast(KotlinJsonAdapterFactory())
             .build()
+
+        val relays = Relays(
+            OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+                .build(),
+            Scarlet.Builder()
+                .addMessageAdapterFactory(MoshiMessageAdapter.Factory(moshi))
+                .addStreamAdapterFactory(RxJava2StreamAdapterFactory())
+                .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
         )
         val relay = relays.relay("wss://relay.damus.io").connectAndSubscribe()
-        val events = relay.flowNotes().take(3).toList()
+        val events = relay.flowRelayMessages().take(3).toList()
         events.forEach { println(it) }
         events shouldHaveSize 3
     }
