5d22820b922da3bf364b5ca2b8c254827f585a7a
Jem Mawson <jem.mawson@gmail.com>
2023-01-16 16:43:15 +1000

    jem/20230116 user meda data repository test (#46)

---
+++ app/src/test/kotlin/social/plasma/relay/BuildingBlocks.kt
@@ -0,0 +1,31 @@
+package social.plasma.relay
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.streamadapter.rxjava2.RxJava2StreamAdapterFactory
+import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import social.plasma.relay.message.NostrMessageAdapter
+
+object BuildingBlocks {
+
+    val moshi = Moshi.Builder()
+        .add(NostrMessageAdapter())
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE))
+        .build()
+
+    val scarlet = Scarlet.Builder()
+        .addMessageAdapterFactory(MoshiMessageAdapter.Factory(moshi))
+        .addStreamAdapterFactory(RxJava2StreamAdapterFactory())
+        .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
+
+    const val JemPubKey = "8366029071b385def2e4fb964d2d73e6f4246131ac1ff7608bbcb1971c5081d2"
+
+}
