b99072b065df6faad5aa61714c15d41a6fab7261
Jose Mateo <jmateo6@gmail.com>
2023-01-18 09:21:13 -0500

    Add image caching, svg, and gif support

--- app/src/main/kotlin/social/plasma/PlasmaAndroidApplication.kt
+++ app/src/main/kotlin/social/plasma/PlasmaAndroidApplication.kt
@@ -1,7 +1,12 @@
 package social.plasma
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PlasmaAndroidApplication : Application()
+class PlasmaAndroidApplication : Application(), ImageLoaderFactory {
+    private val imageLoaderFactory = PlasmaImageLoaderFactory(this)
+    override fun newImageLoader(): ImageLoader = imageLoaderFactory.newImageLoader()
+}
