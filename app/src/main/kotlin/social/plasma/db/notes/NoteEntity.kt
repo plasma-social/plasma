82193167b71679d3adf98396a2583f547e2fabac
Jose Mateo <jmateo6@gmail.com>
2023-01-18 19:28:17 -0500

    Optimize feed db queries

--- app/src/main/kotlin/social/plasma/db/notes/NoteEntity.kt
+++ app/src/main/kotlin/social/plasma/db/notes/NoteEntity.kt
@@ -2,9 +2,15 @@ package social.plasma.db.notes
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "notes")
+@Entity(
+    tableName = "notes",
+    indices = [
+        Index("created_at", "id", orders = [Index.Order.DESC, Index.Order.ASC])
+    ]
+)
 data class NoteEntity(
     @PrimaryKey
     val id: String,
