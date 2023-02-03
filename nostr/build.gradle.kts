6309249b34554880ce3618033c1f0202c0c97b27
Jem Mawson <jem.mawson@gmail.com>
2023-02-04 07:05:33 +1000

    start a relay via docker during testing (#107)

--- nostr/build.gradle.kts
+++ nostr/build.gradle.kts
@@ -1,6 +1,13 @@
+import com.bmuschko.gradle.docker.internal.services.DockerClientService
+import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+import com.bmuschko.gradle.docker.tasks.container.*
+import com.bmuschko.gradle.docker.tasks.image.*
+import java.net.ServerSocket
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.gradle.docker)
     kotlin("kapt")
 }
 
@@ -75,4 +82,43 @@ android.testOptions {
         }
         it.useJUnitPlatform()
     }
+}
+
+val createDockerfile by tasks.creating(Dockerfile::class) {
+    from("scsibug/nostr-rs-relay:latest")
+}
+
+val buildImage by tasks.creating(DockerBuildImage::class) {
+    dependsOn(createDockerfile)
+    images.add("scsibug/nostr-rs-relay:latest")
+}
+
+val createContainer by tasks.creating(DockerCreateContainer::class) {
+    dependsOn(buildImage)
+    targetImageId(buildImage.imageId)
+    hostConfig.portBindings.set(listOf("7707:8080"))
+    hostConfig.autoRemove.set(true)
+}
+
+val startContainer by tasks.creating(DockerStartContainer::class) {
+    this.onlyIf { _ ->
+        try {
+            val s = ServerSocket(7707)
+            s.close()
+            true
+        } catch (_: java.io.IOException) {
+            false
+        }
+    }
+    dependsOn(createContainer)
+    targetContainerId(createContainer.containerId)
+}
+
+val stopContainer by tasks.creating(DockerStopContainer::class) {
+    targetContainerId(createContainer.containerId)
+}
+
+tasks.withType<Test>().configureEach {
+    dependsOn(startContainer)
+    finalizedBy(stopContainer)
 }
