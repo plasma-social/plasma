05e7f8548969117c4d73ec79f557963a2c88bf8f
Jem Mawson <jem.mawson@gmail.com>
2023-02-08 16:29:13 +1000

    unify key generator classes (#120)

--- nostr/src/main/kotlin/social/plasma/crypto/KeyGenerator.kt
+++ nostr/src/main/kotlin/social/plasma/crypto/KeyGenerator.kt
@@ -1,7 +1,25 @@
 package social.plasma.crypto
 
-interface KeyGenerator {
+import okio.ByteString.Companion.toByteString
+import java.security.SecureRandom
 
-    fun generateKey(): KeyPair
+class KeyGenerator {
 
+    fun generateKey(): KeyPair {
+        val bs = ByteArray(32)
+        SecureRandom().nextBytes(bs)
+        val sec = bs.toByteString()
+        if (sec.hex() > MAX_SEC) {
+            return generateKey()
+        }
+        return KeyPair(
+            Bech32.pubkeyCreate(bs).toByteString(),
+            sec
+        )
+    }
+
+    companion object {
+        private const val MAX_SEC =
+            "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"
+    }
 }
