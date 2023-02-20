44635e15963255b9eaf9cb5d5603d991c5f75871
Jose Mateo <jmateo6@gmail.com>
2023-02-20 17:43:24 -0500

    Pipe feed ui events through onEvent (#136)

--- app/src/main/kotlin/social/plasma/ui/feed/FeedUiEvent.kt
+++ app/src/main/kotlin/social/plasma/ui/feed/FeedUiEvent.kt
@@ -1,3 +1,20 @@
 package social.plasma.ui.feed
 
-sealed interface FeedUiEvent
+import social.plasma.models.NoteId
+import social.plasma.models.PubKey
+
+sealed interface FeedUiEvent {
+    data class OnNoteDisposed(
+        val noteId: NoteId,
+        val pubKey: PubKey,
+    ) : FeedUiEvent
+
+    data class OnNoteDisplayed(
+        val noteId: NoteId,
+        val pubKey: PubKey,
+    ) : FeedUiEvent
+
+    data class OnNoteReaction(
+        val noteId: NoteId,
+    ) : FeedUiEvent
+}
