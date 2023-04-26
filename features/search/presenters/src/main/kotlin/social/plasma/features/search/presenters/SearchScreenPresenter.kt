7deb9abb66617231af6c310af5dead60dd857b1c
Jose Mateo <jmateo6@gmail.com>
2023-04-26 19:40:41 -0400

    Add back arrow to collapse search suggestions, and icon to clear search query

--- features/search/presenters/src/main/kotlin/social/plasma/features/search/presenters/SearchScreenPresenter.kt
+++ features/search/presenters/src/main/kotlin/social/plasma/features/search/presenters/SearchScreenPresenter.kt
@@ -3,6 +3,7 @@ package social.plasma.features.search.presenters
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.slack.circuit.Navigator
@@ -11,6 +12,8 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import social.plasma.features.search.screens.SearchBarUiState
+import social.plasma.features.search.screens.SearchBarUiState.LeadingIcon
+import social.plasma.features.search.screens.SearchBarUiState.TrailingIcon
 import social.plasma.features.search.screens.SearchUiEvent
 import social.plasma.features.search.screens.SearchUiState
 import social.plasma.features.search.screens.Suggestion
@@ -43,16 +46,31 @@ class SearchScreenPresenter @AssistedInject constructor(
         var query by rememberSaveable { mutableStateOf("") }
         var isActive by rememberSaveable { mutableStateOf(false) }
 
+        val leadingIcon =
+            remember(isActive) { if (isActive) LeadingIcon.Back else LeadingIcon.Search }
+
+        val trailingIcon =
+            remember(query) { if (query.isEmpty()) null else TrailingIcon.Clear }
+
         return SearchUiState(searchBarUiState = SearchBarUiState(
             query = query,
             isActive = isActive,
-            suggestionsTitle = if(isActive) "RECENT" else null,
-            suggestions = if(isActive) fakeRecentSuggestions else emptyList(),
+            suggestionsTitle = if (isActive) "RECENT" else null,
+            suggestions = if (isActive) fakeRecentSuggestions else emptyList(),
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
         ), onEvent = { event ->
             when (event) {
                 is SearchUiEvent.OnActiveChanged -> isActive = event.active
                 is SearchUiEvent.OnQueryChanged -> query = event.query
                 SearchUiEvent.OnSearch -> isActive = false
+                SearchUiEvent.OnLeadingIconTapped -> isActive = when (leadingIcon) {
+                    LeadingIcon.Back -> false
+                    LeadingIcon.Search -> true
+                }
+
+                SearchUiEvent.OnTrailingIconTapped -> if (trailingIcon == TrailingIcon.Clear) query =
+                    ""
             }
         })
     }
--- features/search/screens/src/main/kotlin/social/plasma/features/search/screens/SearchUiEvent.kt
+++ features/search/screens/src/main/kotlin/social/plasma/features/search/screens/SearchUiEvent.kt
@@ -4,7 +4,10 @@ import com.slack.circuit.CircuitUiEvent
 
 sealed interface SearchUiEvent : CircuitUiEvent {
     object OnSearch : SearchUiEvent
-
     data class OnQueryChanged(val query: String) : SearchUiEvent
     data class OnActiveChanged(val active: Boolean) : SearchUiEvent
+
+    object OnLeadingIconTapped : SearchUiEvent
+
+    object OnTrailingIconTapped : SearchUiEvent
 }
