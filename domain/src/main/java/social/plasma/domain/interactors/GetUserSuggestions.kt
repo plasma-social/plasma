11ed2c0a6d9218ce36e1ed616c9ccb9c0d1073d1
Jose Mateo <jmateo6@gmail.com>
2023-05-06 08:47:57 -0400

    Improve profiles search speed

--- domain/src/main/java/social/plasma/domain/interactors/GetUserSuggestions.kt
+++ domain/src/main/java/social/plasma/domain/interactors/GetUserSuggestions.kt
@@ -1,70 +1,93 @@
 package social.plasma.domain.interactors
 
 import app.cash.nostrino.crypto.PubKey
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.BufferOverflow.DROP_OLDEST
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
 import okio.ByteString.Companion.decodeHex
 import social.plasma.domain.SubjectInteractor
 import social.plasma.models.TagSuggestion
 import social.plasma.shared.repositories.api.UserMetadataRepository
 import javax.inject.Inject
+import javax.inject.Named
+import kotlin.coroutines.CoroutineContext
 
 class GetUserSuggestions @Inject constructor(
     private val userMetadataRepository: UserMetadataRepository,
     private val getNip5Status: GetNip5Status,
+    @Named("default") private val coroutineContext: CoroutineContext,
 ) : SubjectInteractor<GetUserSuggestions.Params, List<TagSuggestion>>() {
     data class Params(val query: String)
 
-    override fun createObservable(params: Params): Flow<List<TagSuggestion>> = flow {
-        if (params.query.isEmpty()) {
-            emit(emptyList())
-            return@flow
-        }
+    private var currentJob: Job? = null
 
-        val query = if (params.query.startsWith("@")) params.query.drop(1) else params.query
+    override fun createObservable(params: Params): Flow<List<TagSuggestion>> {
+        currentJob?.cancel() // Cancel any ongoing operations from the previous query
+        val tagSuggestionsChannel = Channel<List<TagSuggestion>>(onBufferOverflow = DROP_OLDEST)
 
-        if (query.isNotEmpty()) {
-            val userEntities = userMetadataRepository.searchUsers(query)
+        currentJob = CoroutineScope(coroutineContext).launch {
+            val query = if (params.query.startsWith("@")) params.query.drop(1) else params.query
 
-            val tagSuggestions = userEntities.map {
-                TagSuggestion(
-                    pubKey = PubKey(it.pubkey.decodeHex()),
-                    imageUrl = it.picture,
-                    title = it.userFacingName,
-                    nip5Identifier = it.nip05,
-                    isNip5Valid = null,
-                )
+            if (query.isNotEmpty()) {
+                val userEntities = userMetadataRepository.searchUsers(query)
+
+                val tagSuggestions = userEntities.map { userEntity ->
+                    TagSuggestion(
+                        pubKey = PubKey(userEntity.pubkey.decodeHex()),
+                        imageUrl = userEntity.picture,
+                        title = userEntity.userFacingName,
+                        nip5Identifier = userEntity.nip05,
+                        isNip5Valid = null,
+                    )
+                }
+
+                tagSuggestionsChannel.sendWithNip5Validation(tagSuggestions)
+            } else {
+                tagSuggestionsChannel.send(emptyList())
+                tagSuggestionsChannel.close()
             }
-
-            emitTagSuggestions(tagSuggestions)
-        } else {
-            emit(emptyList())
         }
+
+        return tagSuggestionsChannel.consumeAsFlow()
     }
 
 
-    private suspend fun FlowCollector<List<TagSuggestion>>.emitTagSuggestions(tagSuggestions: List<TagSuggestion>) {
+    private suspend fun SendChannel<List<TagSuggestion>>.sendWithNip5Validation(tagSuggestions: List<TagSuggestion>) {
+        send(tagSuggestions)
+
         val tagSuggestionsMap = tagSuggestions.associateBy { it.pubKey }.toMutableMap()
-        emit(tagSuggestionsMap.values.toList())
 
-        tagSuggestionsMap.values.forEach { suggestion ->
-            val nip5Identifier = suggestion.nip5Identifier ?: return@forEach
+        coroutineScope {
+            tagSuggestionsMap.values.forEach { suggestion ->
+                launch {
+                    val nip5Identifier = suggestion.nip5Identifier ?: return@launch
 
-            if (nip5Identifier.trim().isEmpty()) return@forEach
+                    if (nip5Identifier.trim().isEmpty()) return@launch
 
-            val nip5Status = getNip5Status.executeSync(
-                GetNip5Status.Params(
-                    suggestion.pubKey,
-                    nip5Identifier
-                )
-            ).isValid()
+                    val nip5Status = getNip5Status.executeSync(
+                        GetNip5Status.Params(
+                            suggestion.pubKey,
+                            nip5Identifier
+                        )
+                    ).isValid()
 
-            tagSuggestionsMap[suggestion.pubKey] = suggestion.copy(isNip5Valid = nip5Status)
+                    synchronized(tagSuggestionsMap) {
+                        tagSuggestionsMap[suggestion.pubKey] =
+                            suggestion.copy(isNip5Valid = nip5Status)
+                    }
 
-            emit(tagSuggestionsMap.values.toList())
+                    send(tagSuggestionsMap.values.toList())
+                }
+            }
         }
     }
 }
+
 
 
